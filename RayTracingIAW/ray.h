e9936ac572d2ed37bf94cd7ea8f5fa8a53c6d291
TX <5372446@qq.com>
2019-11-21 19:21:35 +0800

    raytracing

---
+++ RayTracingIAW/ray.h
@@ -0,0 +1,19 @@
+#ifndef RAYH
+#define RAYH
+#include "vec3.h"
+
+class ray
+{
+public:
+	ray() {}
+	ray(const vec3& a, const vec3& b) { A = a; B = b; }
+	vec3 origin() const { return A; }
+	vec3 direction() const { return B; }
+	vec3 point_at_parameter(float t) const { return A + t * B; }
+
+	vec3 A;   //起始点位置
+	vec3 B;	  //光线方向
+};
+
+#endif 
+
